edadc07059db40068a6d408043b85213f1cbd2ba
Tyago Nunes <tyagonunes@gmail.com>
2020-04-19 16:13:30 -0300

    pages

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,64 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <div />
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-olympikus-breed-2/02/D22-3195-002/D22-3195-002_zoom2.jpg?ts=1571247200&ims=326x" alt=""/>
+            </td>
+            <td>
+              <strong>Tenis</strong>
+              <span>R$ 20,90</span>
+            </td>
+            <td>
+              <button>
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+              <input type="number" readOnly value={1}/>
+              <button>
+                <MdAddCircleOutline size={20} color="#7159c1" />
+              </button>
+            </td>
+            <td>
+              <strong>R$ 202,00</strong>
+            </td>
+            <td>
+              <button>
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      
+      <footer>
+        <button>Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1900,00</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-olympikus-breed-2/02/D22-3195-002/D22-3195-002_zoom2.jpg?ts=1571247200&ims=326x" alt=""/>
+        <strong>Tenis</strong>
+        <span>R$ 1209,90</span>
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
