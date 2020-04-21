4935442a24274557c8ef709e374a7774cb32fcbf
Tyago Nunes <tyagonunes@gmail.com>
2020-04-21 11:57:30 -0300

    server fake e axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { MdShoppingCart } from 'react-icons/md'
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
+import api from "../../services/api";
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-olympikus-breed-2/02/D22-3195-002/D22-3195-002_zoom2.jpg?ts=1571247200&ims=326x" alt=""/>
-        <strong>Tenis</strong>
-        <span>R$ 1209,90</span>
-        <button>
-          <div>
-            <MdShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+
+export default class Home extends React.Component {
+  state = {
+    products: []
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }))
+
+    this.setState({ products: data })
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button>
+              <div>
+                <MdShoppingCart size={16} color="#fff" />
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
