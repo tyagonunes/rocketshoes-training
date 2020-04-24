128146e97e8da99fe2008ea5bb6a14ff68dc3cac
Tyago Nunes <tyagonunes@gmail.com>
2020-04-23 21:31:14 -0300

    incrementa e decrementa unidades

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -3,11 +3,19 @@ export function addToCart(product) {
     type: '@cart/ADD',
     product,
   };
-}
+};
 
 export function removeFromCart(id) {
   return {
     type: '@cart/REMOVE', 
     id,
+  }
+};
+
+export function updateAmount(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT',
+    id,
+    amount
   }
 }
