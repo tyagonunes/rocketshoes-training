5add14449e2d354f8bd49ebcc3615943b5c948a1
Tyago Nunes <tyagonunes@gmail.com>
2020-04-27 21:39:42 -0300

    navegação por history

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,6 +1,7 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../util/format';
 import { addToCartSuccess, updateAmountSuccess } from './actions'
 
@@ -36,6 +37,8 @@ function* addToCart({ id }) {
       priceFormatted: formatPrice(response.data.price),
     };    
     yield put(addToCartSuccess(data));
+
+    history.push('/cart');
   };
 
 };
